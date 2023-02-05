9c4292671d44edbdc8e00aa392221fbedc0c832e
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-02-05 23:24:08 +0100

    Update OpenAIEndpoint.swift
    Created imageGenerations Endpoint case

--- Sources/OpenAISwift/OpenAIEndpoint.swift
+++ Sources/OpenAISwift/OpenAIEndpoint.swift
@@ -7,6 +7,7 @@ import Foundation
 enum Endpoint {
     case completions
     case edits
+    case imagesGenerations
 }
 
 extension Endpoint {
@@ -16,19 +17,21 @@ extension Endpoint {
             return "/v1/completions"
         case .edits:
             return "/v1/edits"
+        case .imagesGenerations:
+            return "/v1/images/generations"
         }
     }
     
     var method: String {
         switch self {
-        case .completions, .edits:
+        case .completions, .edits, .imagesGenerations:
             return "POST"
         }
     }
     
     func baseURL() -> String {
         switch self {
-        case .completions, .edits:
+        case .completions, .edits, .imagesGenerations:
             return "https://api.openai.com"
         }
     }
